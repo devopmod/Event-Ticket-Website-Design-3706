7e1ee1ed6814c707c2c92be7e7035138b653fbdd
devopmod <devopmod@gmail.com>
2025-07-24 23:48:32 +0200

    Delete database directory

--- database/add_price_book_column.sql
+++
@@ -1,9 +0,0 @@
--- Add price_book column to events table
-ALTER TABLE events_fanaticka_7a3x9d 
-ADD COLUMN IF NOT EXISTS price_book JSONB DEFAULT '{}';
-
--- Create index for price_book queries
-CREATE INDEX IF NOT EXISTS idx_events_price_book ON events_fanaticka_7a3x9d USING GIN (price_book);
-
--- Add comment
-COMMENT ON COLUMN events_fanaticka_7a3x9d.price_book IS 'JSONB object storing category prices for the event';
