1840b539a1a70dd4f629bf0203fd615676eac6d9
devopmod <devopmod@gmail.com>
2025-07-26 00:20:01 +0200

    Delete src directory

--- src/services/seatMapService.js
+++
@@ -1,9 +0,0 @@
-// Этот файл дублирует функциональность из eventService.js и venueService.js
-// Удаляем его, так как:
-// 1. fetchSeatMapData - дублирует fetchEventById + fetchVenueById
-// 2. holdSeat/releaseSeats - дублирует функции из eventService.js  
-// 3. cleanupExpiredReservations - дублирует функцию из venueService.js
-// 4. initializeSeatMapRealtime - дублирует initializeRealtimeSubscription
-
-// Этот файл можно безопасно удалить
-console.warn('seatMapService.js is deprecated. Use eventService.js and venueService.js instead.');
